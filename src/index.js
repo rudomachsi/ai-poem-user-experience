6bd6c0612434c642f64c3fec766e9d6771681735
Rudo Machisi <rudomachisi@gmail.com>
2026-01-16 18:51:31 +0200

    corrected the js code

--- src/index.js
+++ src/index.js
@@ -22,8 +22,8 @@ function generatePoem(event) {
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   
   let poemElement= document.querySelector("#poem");
-  poemElement.classList.remove= "hidden";
-  poemElement.innerHTML= ` <div class="generating>" 👩🏻‍💻 Generating a French poem about ${instructionsInput.value}`
+  poemElement.classList.remove= ("hidden");
+  poemElement.innerHTML= ` <div class="generating"> 👩🏻‍💻 Generating a French poem about ${instructionsInput.value} </div>`;
   
   //Make a call to the API
 
